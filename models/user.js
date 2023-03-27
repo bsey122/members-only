ec3a44c66df8a4510f293f31d3e563b2e6813066
Brandon Seymour <bseymour122@yahoo.ca>
2023-03-27 16:57:01 -0400

    Add password property to User model

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const UserSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   username: { type: String, required: true },
+  password: { type: String, required: true },
   member: { type: Boolean },
   admin: { type: Boolean, default: false },
 });
