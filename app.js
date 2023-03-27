8e6d4ef275e008be356d6e339cc0155378b276f2
Brandon Seymour <bseymour122@yahoo.ca>
2023-03-27 18:42:26 -0400

    Import middleware for authentication

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 require("dotenv").config();
 
 var indexRouter = require("./routes/index");
