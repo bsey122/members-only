e7520fceb76baf15df70d4788115e449c4a13826
Brandon Seymour <bseymour122@yahoo.ca>
2023-03-28 16:40:29 -0400

    Add loginRouter

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ require("dotenv").config();
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const signupRouter = require("./routes/signup");
+const loginRouter = require("./routes/login");
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/sign-up", signupRouter);
+app.use("/login", loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
