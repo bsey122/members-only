45553c6d0b4f5f73c17c4d7509653c9797f492d4
Brandon Seymour <bseymour122@yahoo.ca>
2023-03-30 18:12:53 -0400

    Add functions to update User to member

---
+++ controllers/userController.js
@@ -0,0 +1,20 @@
+const User = require("../models/user");
+
+// Display member form on Get
+exports.member_update_get = function (req, res, next) {
+  res.render("layout", { title: "Become a Member", content: "member" });
+};
+
+// Handle User update on Post
+exports.member_update_post = async (req, res, next) => {
+  if (req.body.passcode !== process.env.PASSCODE) {
+    res.render("layout", { title: "Become a Member", content: "member" });
+    return;
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.user._id, { member: true });
+    res.redirect("/");
+  } catch (error) {
+    return next(error);
+  }
+};
