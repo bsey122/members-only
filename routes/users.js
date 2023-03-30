94c4229b6153aedd425a08d4f59b3fec1ed938fa
Brandon Seymour <bseymour122@yahoo.ca>
2023-03-30 18:15:57 -0400

    Add Get and Post routes for member form

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+// Require controller module
+const userRouter = require("../controllers/userController");
+// Get request to update User to member
+router.get("/member", userRouter.member_update_get);
+
+// Post request to update User
+router.post("/member", userRouter.member_update_post);
+
+module.exports = router;
