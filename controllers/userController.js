82ae9542a0c25ff8d177b732512d4f51267ae8d3
Brandon Seymour <bseymour122@yahoo.ca>
2023-03-31 17:18:57 -0400

    Add function to update to admin on Post request

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,3 +50,35 @@ exports.redirect_to_login = function (req, res, next) {
 exports.admin_update_get = function (req, res, next) {
   res.render("layout", { title: "Admin", content: "admin" });
 };
+
+// Handle User update to admin on Post
+exports.admin_update_post = [
+  // Validate fields
+  body("passcode").custom((value) => {
+    if (value !== process.env.ADMIN_PASSCODE) {
+      throw new Error("The passcode is incorrect");
+    }
+    return true;
+  }),
+  // Process request after validation
+  async (req, res, next) => {
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again
+      res.render("layout", {
+        title: "Admin",
+        content: "admin",
+        errors: errors.array(),
+      });
+      return;
+    }
+    try {
+      // Data from form is valid. Update the record
+      const user = await User.findByIdAndUpdate(req.user._id, { admin: true });
+      res.redirect("/");
+    } catch (error) {
+      return next(error);
+    }
+  },
+];
