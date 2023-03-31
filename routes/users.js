a9291b4df0abc89f39e76b2e19924d88afc61771
Brandon Seymour <bseymour122@yahoo.ca>
2023-03-31 17:20:35 -0400

    Add Post route for admin form

--- routes/users.js
+++ routes/users.js
@@ -15,4 +15,7 @@ router.post("/member", userRouter.member_update_post);
 
 // Get request to update User to admin
 router.get("/admin", userRouter.redirect_to_login, userRouter.admin_update_get);
+
+// Post request to update User to admin
+router.post("/admin", userRouter.admin_update_post);
 module.exports = router;
