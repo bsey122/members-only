ed11d9cca0f2267295ac60bbef7d54af4fe9b297
Brandon Seymour <bseymour122@yahoo.ca>
2023-04-03 11:38:10 -0400

    Remove function to redirect to login page

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,14 +38,6 @@ exports.member_update_post = [
   },
 ];
 
-// Redirect to login page if User is not logged in
-exports.redirect_to_login = function (req, res, next) {
-  if (req.user === undefined) {
-    res.redirect("/login");
-  }
-  next();
-};
-
 // Display admin form on Get
 exports.admin_update_get = function (req, res, next) {
   res.render("layout", { title: "Admin", content: "admin" });
