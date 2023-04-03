2a3a649245a783495596447fddf1d89a98a014c2
Brandon Seymour <bseymour122@yahoo.ca>
2023-04-03 11:39:18 -0400

    Remove redirect middleware function

--- routes/users.js
+++ routes/users.js
@@ -4,17 +4,13 @@ var router = express.Router();
 // Require controller module
 const userRouter = require("../controllers/userController");
 // Get request to update User to member
-router.get(
-  "/member",
-  userRouter.redirect_to_login,
-  userRouter.member_update_get
-);
+router.get("/member", userRouter.member_update_get);
 
 // Post request to update User
 router.post("/member", userRouter.member_update_post);
 
 // Get request to update User to admin
-router.get("/admin", userRouter.redirect_to_login, userRouter.admin_update_get);
+router.get("/admin", userRouter.admin_update_get);
 
 // Post request to update User to admin
 router.post("/admin", userRouter.admin_update_post);
